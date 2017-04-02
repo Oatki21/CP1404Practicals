ade7dc10b2ca0cd6c5c41fe4d1de4de8eee082b9
Oliver <oliver.atkinson@my.jcu.edu.au>
2017-04-02 22:21:46 +1000

    Finished number picks, added gitignore

--- Prac04/lotteryTicketGeneratorV2.py
+++ Prac04/lotteryTicketGeneratorV2.py
@@ -1,12 +1,19 @@
 import random
-
+numbers_generated = [0,0,0,0,0,0]
 
 def main():
-    num_of_picks = int(input("How many quick picks? "))
-    for i in range(num_of_picks):
-        print("{:3} {:3} {:3} {:3} {:3} {:3}".format(random.randrange(0, 46), random.randrange(0, 46),
-                                                     random.randrange(0, 46), random.randrange(0, 46),
-                                                     random.randrange(0, 46), random.randrange(0, 46), ))
+    pick_num = int(input("How many picks?"))
+    count = 0
+    while count < pick_num:
+        for i in range(6):
+            num = random.randint(1,46)
+            if num in numbers_generated:
+                num = random.randint(1,46)
+            numbers_generated[i] = num
+        numbers_generated.sort()
+        print("{}".format(numbers_generated))
+
+        count += 1
 
 
 main()
