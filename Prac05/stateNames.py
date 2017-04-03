545358b149119ab0def75224280f3674f4dd5818
jc428928 <olver.atkinson@my.jcu.edu.au>
2017-04-03 11:01:24 +1000

    Prac 05 Code Reviews with Pull Requests

---
+++ Prac05/stateNames.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+"""
+
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+# print(STATE_NAMES)
+for i in STATE_NAMES:
+    print("{:3} is {:3}".format(i,STATE_NAMES[i]))
+
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in STATE_NAMES:
+        print(state, "is", STATE_NAMES[state])
+    else:
+        print("Invalid short state")
+    state = input("Enter short state: ").upper()
