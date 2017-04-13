7c9d6e7be22b03586bd935d58dd9d486a9604c45
jc428928 <oliver.atkinson@my.jcu.edu.au>
2017-04-13 12:19:11 +1000

    Ensure Miles to Kilometres is complete

--- Prac06/my_convert_m_to_km.py
+++ Prac06/my_convert_m_to_km.py
@@ -1,29 +1,40 @@
-"""
-CP1404/CP5632 Practical 06
-Kivy GUI program to convert miles to kilometres
-Oliver Atkinson, IT@JCU
-Started 10/04/2017
-"""
-
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+
 __author__ = 'Oliver Atkinson'
 
+MILES_TO_KM = 1.60934
 
-class MilestoKilometresApp(App):
-    """ MilestoKilometresApp is a Kivy App for converting Miles to Kilometres """
+
+class MilesConverterApp(App):
+    """Opens the class through Kivvy's Parent App Object"""
+
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('my_convert_m_to_km.kv')
         return self.root
 
-    def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
+    def handle_calculate(self):
+        """Passes value through separate function before calculating and
+         printing result on label"""
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
+    def handle_increment(self, change):
+        """Associated with Up/Down Buttons, a value is passed depending on the button
+        before being applied to currently value in textbox"""
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
 
-MilestoKilometresApp().run()
+    def get_validated_miles(self):
+        """If value passed in isn't compatible as a float type, return 0"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+
+MilesConverterApp().run()
